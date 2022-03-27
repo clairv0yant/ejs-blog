04c2ef84957e077439d483d86d7d41ca299b0547
Luca Mueller <luca@LMacBookPro.local>
2022-03-27 20:49:48 +0200

    add text on home and header/footer on contact/about

--- app.js
+++ app.js
@@ -14,8 +14,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/', function (req, res) {
-  res.render('home');
-})
+  res.render('home', {
+    homeContent: homeStartingContent,
+  });
+});
+
+app.get('/about', function (req, res) {
+  res.render('about');
+});
+
+app.get('/contact', function (req, res) {
+  res.render('contact');
+});
 
 
 app.listen(3000, function() {
