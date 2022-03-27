67cf12cdad19fa0355cb88c9c10363fc45157580
Luca Mueller <luca@LMacBookPro.local>
2022-03-27 21:19:10 +0200

    add input & textarea to form + add styling

--- app.js
+++ app.js
@@ -35,6 +35,11 @@ app.get('/compose', function (req, res) {
   res.render('compose');
 });
 
+app.post('/compose', function (req, res) {
+  let text = req.body.newEntry;
+  console.log(text);
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
